3797f2d86f953eb2d29ca0ca01b6246b5b04653f
mehnaz03 <mehnazuiu@gmail.com>
2020-09-15 21:59:22 +0600

    Edit and Delete data from list

--- app/src/main/java/com/mehnaz/dailyshoppinglist/HomeActivity.java
+++ app/src/main/java/com/mehnaz/dailyshoppinglist/HomeActivity.java
@@ -3,35 +3,41 @@ package com.mehnaz.dailyshoppinglist;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-import android.widget.Toolbar;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 import com.mehnaz.dailyshoppinglist.Model.Data;
 
 import java.text.DateFormat;
 import java.util.Date;
-import java.util.zip.Inflater;
+
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -43,14 +49,23 @@ public class HomeActivity extends AppCompatActivity {
     FirebaseRecyclerAdapter<Data,myViewHolder>adapter;
     String uId;
 
+    private TextView totalSumResult;
+
+    private String type;
+    private int amount;
+    private  String note;
+    private String post_key;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
         toolbar = findViewById(R.id.home_toolbar);
-        setActionBar(toolbar);
-        getActionBar().setTitle("Daily Shopping List");
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Daily Shopping List");
+
+        totalSumResult = findViewById(R.id.tvTotalAmount);
 
         mAuth = FirebaseAuth.getInstance();
 
@@ -69,7 +84,33 @@ public class HomeActivity extends AppCompatActivity {
         homeRecycler.setLayoutManager(layoutManager);
         fetch();
         homeRecycler.setAdapter(adapter);
+
+        // Total sum number
+        mDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                int totalammount = 0;
+
+                for(DataSnapshot snap:snapshot.getChildren()){
+
+                    Data data = snap.getValue(Data.class);
+
+                    totalammount+= data.getAmount();
+
+                    String sttotal = String.valueOf(totalammount + " .00 ");
+                    totalSumResult.setText(sttotal);
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
+
+
 
     private void customDialog() {
         AlertDialog.Builder mydialog = new AlertDialog.Builder(HomeActivity.this);
@@ -160,6 +201,19 @@ public class HomeActivity extends AppCompatActivity {
                 holder.setType(model.getType());
                 holder.setNote(model.getNote());
                 holder.setAmount(model.getAmount());
+                holder.itemView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        post_key = getRef(position).getKey();
+                        type = model.getType();
+                        amount = model.getAmount();
+                        note = model.getNote();
+
+                        updateData();
+                    }
+                });
+
             }
 
         };
@@ -207,6 +261,87 @@ public class HomeActivity extends AppCompatActivity {
             mAmount.setText(ammount);
         }
 
+    }
+
+
+    public void updateData(){
+        AlertDialog.Builder mydialog2 = new AlertDialog.Builder(HomeActivity.this);
+        LayoutInflater inflater = LayoutInflater.from(HomeActivity.this);
+        View myview2 = inflater.inflate(R.layout.update_input_field,null);
+
+        AlertDialog dialog2 = mydialog2.create();
+
+        dialog2.setView(myview2);
+
+        final EditText edt_type = myview2.findViewById(R.id.etTypeUpdate);
+        final EditText edt_ammount = myview2.findViewById(R.id.etAmountUpdate);
+        final EditText edt_note = myview2.findViewById(R.id.etNoteUpdate);
+        Button btn_update = myview2.findViewById(R.id.btnUpdate);
+        Button btn_dlt = myview2.findViewById(R.id.btnDelete);
+
+        edt_type.setText(type);
+        edt_type.setSelection(type.length());
+
+        edt_ammount.setText(String.valueOf(amount));
+        edt_ammount.setSelection(String.valueOf(amount).length());
+
+        edt_note.setText(note);
+        edt_note.setSelection(note.length());
+
+
+
+        btn_update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String mType= edt_type.getText().toString().trim();
+                String mAmmount  = edt_ammount.getText().toString().trim();
+                String  mNote = edt_note.getText().toString().trim();
+
+                int intAmount = Integer.parseInt(mAmmount);
+                String date=DateFormat.getDateInstance().format(new Date());
+
+                Data data=new Data(type,intAmount,note,date,post_key);
+
+                mDatabase.child(post_key).setValue(data);
+
+
+                dialog2.dismiss();
+
+
+            }
+        });
+
+        btn_dlt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                mDatabase.child(post_key).removeValue();
+
+                dialog2.dismiss();
+
+            }
+        });
+
+
+        dialog2.show();
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.log_out:
+                mAuth.signOut();
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 
